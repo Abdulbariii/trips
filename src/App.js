55cb9ee8dee1a31426baaf03cab93ff7f6001351
Abdulbari Qaisar <Abdulbari.112917@gmail.com>
2022-07-05 00:01:40 +0300

    Adding local data

--- src/App.js
+++ src/App.js
@@ -6,32 +6,54 @@ import { useEffect, useState } from "react";
 import Modal from "./component/Modal";
 
 function App() {
-  const [trips, setTrips] = useState([]);
-  const [url, setUrl] = useState("http://localhost:3000/trips");
+  const data = [
+    {
+      name: "paris",
+      price: "800$",
+      image:
+        "https://images.unsplash.com/photo-1562243885-2541f1032824?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80",
+      loc: "europe",
+    },
+    {
+      name: "Dubai",
+      price: "400$",
+      image:
+        "https://images.unsplash.com/photo-1546412414-e1885259563a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+      loc: "asia",
+    },
+    {
+      name: "Erbil",
+      price: "300$",
+      image:
+        "https://images.unsplash.com/photo-1608925086961-dbcd276a0e71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      loc: "asia",
+    },
+    {
+      name: "Egypt",
+      price: "500$",
+      image:
+        "https://images.unsplash.com/photo-1539650116574-8efeb43e2750?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      loc: "africa",
+    },
+    {
+      name: "Berlin",
+      price: "900$",
+      image:
+        "https://images.unsplash.com/photo-1560930950-5cc20e80e392?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      loc: "europe",
+    },
+    {
+      name: "London",
+      price: "900$",
+      image:
+        "https://images.unsplash.com/photo-1486299267070-83823f5448dd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+      loc: "europe",
+    },
+  ];
+
+  const [trips, setTrips] = useState(data);
+
   const [modal, setModal] = useState(false);
-
-  const fetchData = async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    setTrips(data);
-  };
-  const asia = "http://localhost:3000/trips?loc=asia";
-  const africa = "http://localhost:3000/trips?loc=africa";
-  const europe = "http://localhost:3000/trips?loc=europe";
-
-  const setAsia = () => {
-    setUrl(asia);
-  };
-  const setAfrica = () => {
-    setUrl(africa);
-  };
-  const setEurope = () => {
-    setUrl(europe);
-  };
-
-  const setAll = () => {
-    setUrl("http://localhost:3000/trips");
-  };
 
   const setTicket = () => {
     setModal(true);
@@ -41,20 +63,51 @@ function App() {
     setModal(false);
   };
 
-  console.log(trips);
+  const locAsia = () => {
+    setTrips(data);
+    setTrips((trips) => {
+      return trips.filter((trips) => {
+        return trips.loc === "asia";
+      });
+    });
+  };
 
-  useEffect(() => {
-    fetchData();
-  }, [url]);
+  const locAfrica = () => {
+    setTrips(data);
+    setTrips((trips) => {
+      return trips.filter((trips) => {
+        return trips.loc === "africa";
+      });
+    });
+  };
+
+  const locEurope = () => {
+    setTrips(data);
+    setTrips((trips) => {
+      return trips.filter((trips) => {
+        return trips.loc === "europe";
+      });
+    });
+  };
+
+  const locAll = () => {
+    setTrips(data);
+    setTrips((trips) => {
+      return trips.filter((trips) => {
+        return trips;
+      });
+    });
+  };
+  console.log(trips.loc);
 
   return (
     <div className="App  flex text-center flex-wrap  mt-14 flex-col items-center justify-center lg:w-[36rem] md:w-[36rem] ">
       <Title />
       <Btn
-        setAsia={setAsia}
-        setAll={setAll}
-        setAfrica={setAfrica}
-        setEurope={setEurope}
+        locAsia={locAsia}
+        locAfrica={locAfrica}
+        locEurope={locEurope}
+        locAll={locAll}
       />
       {trips.map((places) => (
         <Card
