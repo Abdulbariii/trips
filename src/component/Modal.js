7e8fa6922caa859bb944a508c7f5e6c5dd843bf7
Abdulbari Qaisar <Abdulbari.112917@gmail.com>
2022-07-05 02:12:08 +0300

    fixing

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Modal(props) {
   return (
-    <div className="flex backdrop-blur-3xl fixed left-1/2 top-1/2 translate-y-[-50%] translate-x-[-50%] p-10  justify-center items-center flex-col w-80 h-72 bg-slate-900 ">
+    <div className="flex fixed left-1/2 top-1/2 translate-y-[-50%] translate-x-[-50%] p-10  justify-center items-center flex-col w-80 h-72 bg-slate-900 ">
       <h1 className="text-slate-50 text-3xl m-5 ">
         oops <span className="text-3xl text-red-600">!</span>
       </h1>
